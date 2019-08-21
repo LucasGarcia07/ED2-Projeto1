1eed8cfbf86ae5331606be4a4a6822028c9faf74
Noodle <lucasgarciamj@gmail.com>
2019-08-20 22:01:24 -0300

    criação cliente.h

---
+++ cliente.h
@@ -0,0 +1,22 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct cliente{
+    int codCliente;
+    char nome[50];
+    char dataNascimento[20];
+};typedef struct cliente Cliente;
+
+
+
+
+
+
+
+
+
+
+#endif
